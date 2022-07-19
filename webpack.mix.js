b6b53842499b0ad38f014885251baaa44be8bf54
Specialist <darking-uz@yandex.com>
2022-07-19 13:42:49 +0500

    upgrade package.json

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,6 @@ let mix = require('laravel-mix')
 
 mix
     .setPublicPath('dist')
-    .js('resources/js/field.js', 'js')
+    .js('resources/js/field.js', 'js').vue()
     .copy("./node_modules/leaflet/dist/images", "dist/vendor/leaflet/dist/images")
 ;
